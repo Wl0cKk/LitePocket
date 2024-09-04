1cd14080aa2a91168c20bdaff314164754ef27bb
Wl0cKk <kandraciuk404@gmail.com>
2024-09-04 18:01:39 +0300

    Deprecate from buttons to Tabs

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from kivy.lang import Builder
 from kivy.core.window import Window
-from kivy.uix.label import Label
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.button import MDIconButton
 
 class MainLayout(MDBoxLayout):
     pass
@@ -20,8 +18,12 @@ class MainApp(MDApp):
         Window.minimum_height = 240
         self.title = "LitePocket"
         self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "White"
         Builder.load_file('litepocket.kv')
         return MainLayout()
+    
+    def on_start(self):
+        self.root.ids.footer.ids.tabs.switch_tab(text="Receive")
 
 if __name__ == '__main__':
     MainApp().run()
